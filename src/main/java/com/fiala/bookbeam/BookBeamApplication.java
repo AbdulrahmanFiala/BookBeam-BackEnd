e0c0088e172bc5e4c4becde6d856d49df2f1f29a
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-07 08:12:59 +0200

    initial commit

---
+++ src/main/java/com/fiala/bookbeam/BookBeamApplication.java
@@ -0,0 +1,13 @@
+package com.fiala.bookbeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiala/bookbeam/BookbeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiala.bookbeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
