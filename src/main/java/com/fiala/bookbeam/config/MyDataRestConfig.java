5dbfc9a53f8be7efa231c4819c86cf27228bf8de
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-09 03:59:11 +0200

    exposed the ids of review endpoint and disabled all crud ops except read

--- src/main/java/com/fiala/bookbeam/config/MyDataRestConfig.java
+++ src/main/java/com/fiala/bookbeam/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.fiala.bookbeam.config;
 
 import com.fiala.bookbeam.entity.Book;
+import com.fiala.bookbeam.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -16,8 +17,11 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
+
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /*Configure CORS Mapping*/
         cors.addMapping(config.getBasePath() + "/**")
