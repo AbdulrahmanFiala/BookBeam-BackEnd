d6ebf7d02e73b7cf025f25ee78ccd1bc776246a2
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-13 07:30:52 +0200

    Adding history repository service to the book services

--- src/main/java/com/fiala/bookbeam/service/BookService.java
+++ src/main/java/com/fiala/bookbeam/service/BookService.java
@@ -2,8 +2,10 @@ package com.fiala.bookbeam.service;
 
 import com.fiala.bookbeam.dao.BookRepository;
 import com.fiala.bookbeam.dao.CheckoutRepository;
+import com.fiala.bookbeam.dao.HistoryRepository;
 import com.fiala.bookbeam.entity.Book;
 import com.fiala.bookbeam.entity.Checkout;
+import com.fiala.bookbeam.entity.History;
 import com.fiala.bookbeam.responsemodels.ShelfCurrentLoansResponse;
 import lombok.Data;
 import org.springframework.stereotype.Service;
@@ -25,9 +27,12 @@ public class BookService {
 
     private CheckoutRepository checkoutRepository;
 
-    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+    private HistoryRepository historyRepository;
+
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository, HistoryRepository historyRepository) {
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
+        this.historyRepository = historyRepository;
     }
 
     public Book checkoutBook(String userEmail, Long bookId) throws Exception {
@@ -111,6 +116,17 @@ public class BookService {
 
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
+
+        History history = new History(userEmail,
+                validateCheckout.getCheckoutDate(),
+                LocalDate.now().toString(),
+                book.get().getTitle(),
+                book.get().getAuthor(),
+                book.get().getDescription(),
+                book.get().getImg());
+
+        historyRepository.save(history);
+
     }
 
     public void renewLoan (String userEmail, Long bookId) throws Exception{
