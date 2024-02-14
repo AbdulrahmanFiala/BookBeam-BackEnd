68e49b84397efd914e01333ee43db9c929c9ea0e
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-14 14:32:43 +0200

    Add put message method

--- src/main/java/com/fiala/bookbeam/service/MessagesService.java
+++ src/main/java/com/fiala/bookbeam/service/MessagesService.java
@@ -2,9 +2,12 @@ package com.fiala.bookbeam.service;
 
 import com.fiala.bookbeam.dao.MessageRepository;
 import com.fiala.bookbeam.entity.Message;
+import com.fiala.bookbeam.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -20,5 +23,16 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception{
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+        if (!message.isPresent()){
+            throw new Exception("Message not found");
+        }
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 }
