6f9328807cc91d9c6db73e9be63fbb1dbe23c627
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-15 10:46:28 +0200

    Create admin controller
    provide api endpoints for increasing and decreasing book quantities and adding a book.

---
+++ src/main/java/com/fiala/bookbeam/controller/AdminController.java
@@ -0,0 +1,56 @@
+package com.fiala.bookbeam.controller;
+
+import com.fiala.bookbeam.entity.Book;
+import com.fiala.bookbeam.entity.Message;
+import com.fiala.bookbeam.requestmodels.AddBookRequest;
+import com.fiala.bookbeam.service.AdminService;
+import com.fiala.bookbeam.service.MessagesService;
+import com.fiala.bookbeam.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam long bookId)
+            throws Exception{
+        String admin =  ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only.");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam long bookId)
+            throws Exception{
+        String admin =  ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only.");
+        }
+        adminService.decreaseBookQuantity(bookId);
+    }
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token, @RequestBody AddBookRequest addBookRequest)
+            throws Exception{
+        String admin =  ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only.");
+        }
+        adminService.postBook(addBookRequest);
+    }
+
+
+
+}
