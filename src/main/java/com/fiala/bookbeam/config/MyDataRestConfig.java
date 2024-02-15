7534a3a088fe5b081944ab7fadc79eb46ec6582f
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-15 13:00:20 +0200

    Add configuration for https certificates and modifying the path

--- src/main/java/com/fiala/bookbeam/config/MyDataRestConfig.java
+++ src/main/java/com/fiala/bookbeam/config/MyDataRestConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private String[] theAllowedOrigins = { "http://127.0.0.1:3000", "http://localhost:3000" };
+    private String[] theAllowedOrigins = { "https://127.0.0.1:3000", "https://localhost:3000" };
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
