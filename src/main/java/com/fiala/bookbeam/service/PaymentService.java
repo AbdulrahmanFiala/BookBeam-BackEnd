dca38091f937d77147ca888d890036cdb099721e
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-16 05:12:28 +0200

    Create Payment Service

---
+++ src/main/java/com/fiala/bookbeam/service/PaymentService.java
@@ -0,0 +1,24 @@
+package com.fiala.bookbeam.service;
+
+import com.fiala.bookbeam.dao.MessageRepository;
+import com.fiala.bookbeam.dao.PaymentRepository;
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PaymentService {
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository,
+                          @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+        Stripe.apiKey = secretKey;
+    }
+
+
+}
