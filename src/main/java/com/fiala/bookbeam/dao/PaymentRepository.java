3d293ae099e33f579bc956517fd64795613ad805
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-17 19:46:45 +0200

    Removed redundant imports

--- src/main/java/com/fiala/bookbeam/dao/PaymentRepository.java
+++ src/main/java/com/fiala/bookbeam/dao/PaymentRepository.java
@@ -1,11 +1,7 @@
 package com.fiala.bookbeam.dao;
 
-import com.fiala.bookbeam.entity.Message;
 import com.fiala.bookbeam.entity.Payment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Payment findByUserEmail(String userEmail);
